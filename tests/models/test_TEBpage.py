83658979f06038ab56fc29ded4d145d9e5f0b8a7
bp100a <bp100a@hotmail.com>
2018-09-01 11:27:51 -0400

    - revamping to provide "chain of responsibility" pattern - restructured brewerypage object so instantiated objects can be reused. - fix up UTF8 encoding/decoding to be consistent with URL reads and test data

--- tests/models/test_TEBpage.py
+++ tests/models/test_TEBpage.py
@@ -1,15 +1,23 @@
 from unittest import TestCase
-from models.TEB import TEBpage
+from models.breweries.TEB import TEBpage
 
 
 class TestTEBpage(TestCase):
     def test_TEB_read(self):
-        teb = TEBpage()
+        teb = TEBpage(mocked=True)
         assert (teb is not None)
 
     def test_TEB_beerlist(self):
-        teb = TEBpage()
+        teb = TEBpage(mocked=True)
         assert teb is not None
-
-        beer_string = teb.alexa_taplist()
+        teb.fetch_taplist()
+        beer_string = teb.ssml_taplist()
         assert beer_string is not None
+
+        # read our pre-canned response to compare with (../tests/data/<brewery>.SSML)
+        fn = '../data/' + teb._brewery_name.replace(' ', '') + '.SSML'
+        fp = open(fn, mode='r', encoding='utf8')
+        tst_data = fp.read()
+        fp.close()
+        assert (tst_data == beer_string)  # anything different, raise hell!
+        
