c259d42e673edc52b06c6485d071366b10787532
bp100a <bp100a@hotmail.com>
2018-09-11 11:15:31 -0400

    - cleanup some pylint stuff

--- tests/models/test_untapped.py
+++ tests/models/test_untapped.py
@@ -1,27 +1,28 @@
+"""Test cases for pages hosted by UnTappd"""
 from unittest import TestCase
-import os
 from models.breweries.untappd import UnTappdPage
 from models.breweries.untappd import BREWERY_INFO
 from tests.models.common import data_dir
 
 
 class TestUntappdpage(TestCase):
+    """test for pages hosted by UnTappd"""
 
     def test_Untappd_read(self):
         """Test we can access all breweries we know about"""
         for brewery in BREWERY_INFO:
-            ut = UnTappdPage(brewery=brewery, mocked=True)
-            assert ut is not None
-            ut = None
+            untapped_page = UnTappdPage(brewery=brewery, mocked=True)
+            assert untapped_page is not None
+            untapped_page = None
 
     def test_UnTappd_mocked_brewery(self):
         """Test that we cna read brewery pages and validate results"""
-        ut = UnTappdPage(mocked=True)
+        untapped_page = UnTappdPage(mocked=True)
         for brewery_name in BREWERY_INFO:
             # we are mocking the URL, reading a local test file (../tests/data/<brewery>.HTML)
-            assert ut is not None
-            ut.fetch_taplist(brewery=brewery_name)
-            beer_string = ut.ssml_taplist()
+            assert untapped_page is not None
+            untapped_page.fetch_taplist(brewery=brewery_name)
+            beer_string = untapped_page.ssml_taplist()
             assert beer_string is not None
             # read our pre-canned response to compare with (../tests/data/<brewery>.SSML)
             file_name = data_dir() + brewery_name.replace(' ', '') + '.SSML'
@@ -33,38 +34,38 @@ class TestUntappdpage(TestCase):
 
     def test_UnTappd_aliases(self):
         """Test to validate all aliases"""
-        ut = UnTappdPage(mocked=True)
-        assert ut is not None
+        untapped_page = UnTappdPage(mocked=True)
+        assert untapped_page is not None
 
         # see if aliases exist
-        found = ut.brewery_by_alias("TEB")
+        found = untapped_page.brewery_by_alias("TEB")
         assert found is None
 
-        found = ut.brewery_by_alias("Alementary")
+        found = untapped_page.brewery_by_alias("Alementary")
         assert found == "Alementary Brewing"
 
-        found = ut.brewery_by_alias("Alementary Brewery")
+        found = untapped_page.brewery_by_alias("Alementary Brewery")
         assert found == "Alementary Brewing"
 
-        found = ut.brewery_by_alias("Angry Erik Brewing")
+        found = untapped_page.brewery_by_alias("Angry Erik Brewing")
         assert found == "Angry Erik"
 
-        found = ut.brewery_by_alias("Man Skirt")
+        found = untapped_page.brewery_by_alias("Man Skirt")
         assert found == "Man Skirt Brewing"
 
-        found = ut.brewery_by_alias("Man Skirt Brewery")
+        found = untapped_page.brewery_by_alias("Man Skirt Brewery")
         assert found == "Man Skirt Brewing"
 
-        found = ut.brewery_by_alias("Demented")
+        found = untapped_page.brewery_by_alias("Demented")
         assert found == "Demented Brewing"
 
-        found = ut.brewery_by_alias("Demented Brewery")
+        found = untapped_page.brewery_by_alias("Demented Brewery")
         assert found == "Demented Brewing"
 
     def test_shortnames(self):
         """Test we have the proper short names"""
-        ut = UnTappdPage()
-        short_names = ut.short_name()
+        untapped_page = UnTappdPage()
+        short_names = untapped_page.short_name()
         assert 'Man Skirt' in short_names
         assert 'Demented' in short_names
         assert 'Angry Erik' in short_names
