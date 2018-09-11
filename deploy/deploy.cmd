12290ac8d4a8a78aca70857a47e40432008a13be
bp100a <bp100a@hotmail.com>
2018-09-11 13:26:30 -0400

    - fix type in deployment script

--- deploy/deploy.cmd
+++ deploy/deploy.cmd
@@ -1,2 +1,2 @@
 :: Using the AWS CLI (with credentials pre-loaded) upload the zipfile we created
-aws lambda update-fuction-code --function-name TapList --zip-file fileb://../lambda_deploy/taplist.zip
+aws lambda update-function-code --function-name TapList --zip-file fileb://../lambda_deploy/taplist.zip
