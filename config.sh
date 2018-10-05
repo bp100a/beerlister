805a4eb1f89a35d454d13e2e1330722bff82a77d
bp100a <bp100a@hotmail.com>
2018-10-05 10:33:31 -0400

    checks in script

--- config.sh
+++ config.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
 # generate a configuration file from environment variables
 echo \"\"\"secret values known only to DevOps\"\"\" > config.py
 echo "BUILD_NUMBER=" $CIRCLE_BUILD_NUM >> config.py
