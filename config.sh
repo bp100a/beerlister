c07c630b2a908ecac7da9a202076d62056020ef7
bp100a <bp100a@hotmail.com>
2018-10-05 10:25:05 -0400

    cleanup ^M in script file leaving ? at end of file name

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 # generate a configuration file from environment variables
-echo \"\"\"secret values known only to DevOps\"\"\"
+echo \"\"\"secret values known only to DevOps\"\"\" > config.py
 echo "BUILD_NUMBER=" $CIRCLE_BUILD_NUM >> config.py
 echo "REDIS_HOST=" \"$REDIS_HOST\" >> config.py
 echo "REDIS_PORT=" $REDIS_PORT >> config.py
 echo "REDIS_PASSWORD=" \"$REDIS_PASSWORD\" >> config.py
-
