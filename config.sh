233c227a03ee886a77e7c88853203043ccd268f4
bp100a <bp100a@hotmail.com>
2018-10-05 12:41:21 -0400

    remove pipefail option

--- config.sh
+++ config.sh
@@ -6,9 +6,6 @@ set -o nounset
 # Exit script if a statement returns a non-true return value.
 set -o errexit
 
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
-
 # generate a configuration file from environment variables
 echo \"\"\"secret values known only to DevOps\"\"\" > config.py
 echo "BUILD_NUMBER=" $CIRCLE_BUILD_NUM >> config.py
