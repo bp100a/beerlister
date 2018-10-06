221c916964d590b556b18fd4da1de87a92175825
bp100a <bp100a@hotmail.com>
2018-10-06 14:39:37 -0400

    fix up grep string

--- deploy/validate-build.sh
+++ deploy/validate-build.sh
@@ -8,9 +8,9 @@
 
 aws lambda invoke --invocation-type RequestResponse --function-name TapList --qualifier STAGE --region us-east-1 --payload file://tests/data/ListBreweries.json ListBreweries.out
 
-if grep -q 'Here are the breweries that I know' ListBreweries.out; then
+if grep -q 'Here are the breweries I know' ./deploy/ListBreweries.out; then
    exit 0
 fi
 
 # there's something wrong with response, get out
-exit -1
+exit 255
