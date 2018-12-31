1485b6e1571c74721aed6f4277d9d5f8eab315a9
bp100a <bp100a@hotmail.com>
2018-12-31 13:15:13 -0500

    fix conditions

--- deploy/update-alias.sh
+++ deploy/update-alias.sh
@@ -30,8 +30,8 @@ then
    echo "No matching lambda version found for build number $build_number"
    echo "next marker= $next_marker"
 
-   while [$lambda_version == "" and $next_marker != null]
-   do
+   while [$lambda_version == ""] && [$next_marker != null]
+    do
        aws lambda list-versions-by-function --function-name $lambda_name --region $aws_region --marker $next_marker --output json > list.json
        lambda_version=$(cat list.json | jq -r ".Versions[] | select(.Version!=\"\$LATEST\") | select(.Description == \"${build_number}\").Version")
        next_marker=$(cat list.json | jq -r ".NextMarker")
